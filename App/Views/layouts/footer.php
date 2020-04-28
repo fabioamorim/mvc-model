343e895edac8d9a4d5cc2e946d96cdabb2f1cd71
Fábio Batista do Amorim <batista.amorim@hotmail.com>
2020-04-28 06:28:18 -0700

    Add new views

---
+++ App/Views/layouts/footer.php
@@ -0,0 +1,13 @@
+    <footer class="footer">
+        <div class="container">
+            <p class="text-muted">
+                Footer =)
+            </p>
+        </div>
+    </footer>
+
+    <script src="http://<?php echo APP_HOST; ?>/public/jquery/jquery-3.5.0.min.js"></script>
+    <script src="http://<?php echo APP_HOST; ?>/public/js/bootstrap4/bootstrap.js"></script>
+
+</body>
+</html>
